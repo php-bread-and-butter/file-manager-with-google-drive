95d2041ef13743f6400672e34d0dc494df51b9c6
Subhadeep Chowdhury <ch.subhadeep@gmail.com>
2022-03-11 04:39:06 +0530

    Added some messages

--- cli.php
+++ cli.php
@@ -3,9 +3,12 @@ include "UploadException.php";
 include "GoogleDrive.php";
 
 try {
-    if(!isset($argv[1])) throw new Exception("File path is missing. First argument is required. \n", 400);
+    echo "Welcome to Google Drive Manager via CLI\n";
+    echo "=======================================\n\n";
 
-    if(!isset($argv[2])) throw new Exception("Target directory is missing. Second argument is required. \n", 400);
+    if(!isset($argv[1])) throw new Exception("ERROR: File path is missing. First argument is required. \n", 400);
+
+    if(!isset($argv[2])) throw new Exception("ERROR: Target directory is missing. Second argument is required. \n", 400);
 
     $drive = new GoogleDrive();
     $filePath = $argv[1];
@@ -14,15 +17,19 @@ try {
     $drive->targetDirectory = [$folderId];
     if (file_exists($filePath)) 
     {
+        $fileSize = filesize($filePath);
+
+        echo "File found: $filePath($fileSize)\n";
+
         $response = $drive->writeFile(file_get_contents($filePath), $filePath);
         if(isset($response["id"])) 
-            throw new Exception("File uploaded successfully: {$response["id"]}\n", 200);
+            throw new Exception("SUCCESS: File uploaded successfully: {$response["id"]}\n", 200);
         else
-            throw new Exception("Unable to upload file. Try again later.", 403);
+            throw new Exception("ERROR: Unable to upload file. Try again later.", 403);
     }
     else
     {
-        throw new Exception("Source file path not found: $filePath\n", 404);
+        throw new Exception("ERROR: Source file path not found: $filePath\n", 404);
     }
 } catch (\Throwable $th) {
     error_log($th->getMessage(), $th->getCode());
